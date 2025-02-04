be6fad6c285743e4db362239e5073250bf3ee679
puffcode <74677679+puffcode@users.noreply.github.com>
2025-02-04 15:26:52 -0500

    restart

---
+++ static-page/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return React.createElement(
+    "header",
+    null,
+    React.createElement("h1", null, "Cinderella")
+  );
+};
+
+export default Header;
